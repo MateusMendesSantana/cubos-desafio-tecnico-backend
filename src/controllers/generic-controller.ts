6acd339ad94adb0cf8ac0d22e884838a6d289ad7
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-04-24 22:31:11 -0300

    Implementação de métodos ao GenericController

--- src/controllers/generic-controller.ts
+++ src/controllers/generic-controller.ts
@@ -1,9 +1,40 @@
 import { GenericDAO } from "../DAOs/generic-dao";
+import { Base } from "../models/base";
 
-export class GenericController {
-    protected dao: GenericDAO;
+export abstract class GenericController<Model extends Base> {
+    protected dao: GenericDAO<Model>;
 
-    constructor(dao: GenericDAO) {
+    constructor(dao: GenericDAO<Model>) {
         this.dao = dao;
+    }
+
+    async create(req: any, res: any) {
+        const result = this.dao.create(req.body);
+
+        res.send(result);
+    }
+
+    async read(req: any, res: any) {
+        const result = this.dao.read(req.params.id);
+
+        res.send(result);
+    }
+
+    async update(req: any, res: any) {
+        const result = this.dao.update(req.params.id, req.body);
+
+        res.send(result);
+    }
+
+    async delete(req: any, res: any) {
+        const result = this.dao.delete(req.params.id);
+
+        res.send(result);
+    }
+
+    async list(req: any, res: any) {
+        const result = this.dao.list();
+
+        res.send(result);
     }
 }
