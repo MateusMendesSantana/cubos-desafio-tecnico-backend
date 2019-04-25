9e57dbdf77be20a279ba1aec1845dbcb5ab37f6a
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-04-24 22:17:44 -0300

    Criação do modelo schedule

---
+++ src/models/schedule.ts
@@ -0,0 +1,11 @@
+import { Base } from "./base";
+
+export class Schedule extends Base {
+    public day: Date;
+    public intervals: Interval[];
+}
+
+export interface Interval {
+    start: string;
+    end: string;
+}
