8a5057528da2d784de73f61adefebc24049f799e
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-04-24 22:29:06 -0300

    Ajustes no modelo Schedule

--- src/models/schedule.ts
+++ src/models/schedule.ts
@@ -1,8 +1,8 @@
 import { Base } from "./base";
 
 export class Schedule extends Base {
-    public day: Date;
-    public intervals: Interval[];
+    public day!: Date;
+    public intervals!: Interval[];
 }
 
 export interface Interval {
