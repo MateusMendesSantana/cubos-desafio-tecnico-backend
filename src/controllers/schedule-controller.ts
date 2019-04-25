27f858e69fdac09f614c6186a607c85b727eb8e3
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-04-24 22:31:27 -0300

    Criação do ScheduleController

---
+++ src/controllers/schedule-controller.ts
@@ -0,0 +1,6 @@
+import { GenericController } from "./generic-controller";
+import { Schedule } from "../models/schedule";
+
+export class ScheduleController extends GenericController<Schedule> {
+
+}
