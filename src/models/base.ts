6df7993f21631b1bc54f9dbb3e220dfb53640d99
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-04-25 21:45:17 -0300

    Modificação no construtor do modelo Base

--- src/models/base.ts
+++ src/models/base.ts
@@ -3,7 +3,9 @@ import uuidv4 from 'uuid/v4';
 export abstract class Base {
     public id: string;
 
-    constructor() {
+    constructor(data: any) {
         this.id = uuidv4();
+
+        Object.assign(this, data);
     }
 }
