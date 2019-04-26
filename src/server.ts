fa8aafe480046222b507a24f5bad0cd76809be80
Mateus Mendes <mateusmendessantana@hotmail.com>
2019-04-25 21:46:11 -0300

    Adição do body-parser

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import bodyParser from 'body-parser';
 import { router } from "./routes/schedule-routes";
 
 const server = express();
 
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
 server.use(router);
 
 server.get("/", (_, res) => {
